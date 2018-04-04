73e7a65a7f246d6822ef3b36c74eebf833435ffc
world3k <world3k@gmail.com>
2018-04-04 12:35:29 -0500

    first time

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,17 @@
+package ca.demo.salestool;
+
+public class Main {
+
+
+
+public static void main(String[] args){
+	   SalesData data=new SalesData();
+	   displayGreeting();
+	   data.display();  
+	}
+	   private static void displayGreeting(){
+	   System.out.println("Hello Happy Sales People!");
+	  System.out.println("This APP Sows Sales Data");
+	}
+	  
+}
